3d6570c9c52c0a2ca3c5a4c484c1f2b4251d9d9b
Christo K J <christokj2@gmail.com>
2025-06-28 15:11:38 +0530

    Updated backend code for serverless deployment

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -6,23 +6,25 @@ import { ENV } from './config/env';
 const PORT = ENV.PORT;
 const numCPUs = os.cpus().length;
 
-if (process.env.NODE_ENV === 'development') {
-    if (cluster.isPrimary) {
-        console.log(`Primary ${process.pid} is running`);
+// if (process.env.NODE_ENV === 'development') {
+//     if (cluster.isPrimary) {
+//         console.log(`Primary ${process.pid} is running`);
 
-        for (let i = 0; i < numCPUs; i++) {
-            cluster.fork();
-        }
+//         for (let i = 0; i < numCPUs; i++) {
+//             cluster.fork();
+//         }
 
-        cluster.on('exit', (worker) => {
-            console.log(`Worker ${worker.process.pid} died. Restarting...`);
-            cluster.fork();
-        });
-    } else {
-        app.listen(PORT, () => {
-            console.log(`Worker ${process.pid} listening on port ${PORT}`);
-        });
-    }
-}
+//         cluster.on('exit', (worker) => {
+//             console.log(`Worker ${worker.process.pid} died. Restarting...`);
+//             cluster.fork();
+//         });
+//     } else {
+
+//     }
+// }
+app.listen(PORT, () => {
+    console.log(`Worker ${process.pid} listening on port ${PORT}`);
+});
+
 
 export default app;
